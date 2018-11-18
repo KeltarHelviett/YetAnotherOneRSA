bc12e8ea724ee52ef7bf84323e0486e2a4dc733b
KeltarHelviett <keltarhelviett@gmail.com>
2018-11-19 00:48:04 +1000

    Add padding

--- YetAnotherOneRSA/Rsa.cs
+++ YetAnotherOneRSA/Rsa.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Numerics;
 
@@ -20,13 +21,23 @@ namespace YetAnotherOneRSA
             var len = reader.BaseStream.Length;
             var pos = reader.BaseStream.Position;
             var zero = new byte[] { 0x00 };
-            while (pos < len)
+            byte[] bytes;
+            BigInteger c;
+            while (pos < len - 32)
             {
-                var bytes = reader.ReadBytes(32);
-                var c = new BigInteger(bytes);
+                bytes = reader.ReadBytes(32);
+                c = new BigInteger(bytes);
                 bytes = BigInteger.ModPow(c, _d, _n).ToByteArray();
                 writer.Write(bytes);
                 pos += 32;
+            }
+
+            bytes = reader.ReadBytes(32).Concat(zero).ToArray();
+            c = new BigInteger(bytes);
+            bytes = BigInteger.ModPow(c, _d, _n).ToByteArray();
+            if (bytes[7] != 0x08)
+            {
+                writer.Write(bytes, 0, 8 - bytes[7]);
             }
         }
 
@@ -39,13 +50,20 @@ namespace YetAnotherOneRSA
             writer.Write(Extend(_n.ToByteArray()));
             while (pos < len)
             {
-                var bytes = reader.ReadBytes(8).Concat(zero).ToArray();
+                var bytes = reader.ReadBytes(8);
+                bytes = bytes.Length < 8 ? Extend(bytes, 8, (byte)Math.Abs(8 - bytes.Length)) : bytes.Concat(zero).ToArray();
                 var m = new BigInteger(bytes);
                 bytes = BigInteger.ModPow(m, _e, _n).ToByteArray();
                 if (bytes.LongLength < 32)
                     bytes = Extend(bytes);
                 writer.Write(bytes);
                 pos += 8;
+            }
+
+            if (len % 8 == 0)
+            {
+                var m = new BigInteger(Enumerable.Repeat((byte)0x08, 8).Concat(zero).ToArray());
+                writer.Write(Extend(BigInteger.ModPow(m, _e, _n).ToByteArray()));
             }
             writer.Close();
             reader.Close();
