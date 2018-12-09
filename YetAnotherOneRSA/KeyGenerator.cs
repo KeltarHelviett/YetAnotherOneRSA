e029832a3167ed6c15f344963395e00644ce3e6c
KeltarHelviett <keltarhelviett@gmail.com>
2018-12-09 18:12:23 +1000

    Move number utility functions to NumberTheoryUtils

--- YetAnotherOneRSA/KeyGenerator.cs
+++ YetAnotherOneRSA/KeyGenerator.cs
@@ -5,48 +5,20 @@ namespace YetAnotherOneRSA
 {
     public static class KeyGenerator
     {
-        private static readonly RandomNumberGenerator _rng = RandomNumberGenerator.Create();
-
         public static (BigInteger, BigInteger, BigInteger) GetRsaKeys()
         {
-            var p = RandomPrime();
-            var q = RandomPrime();
+            var p = NumberTheoryUtils.RandomPrime();
+            var q = NumberTheoryUtils.RandomPrime();
             var n = p * q;
             var phi = (p - 1) * (q - 1);
-            var e = RandomIntegerInRange(2, phi);
+            var e = NumberTheoryUtils.RandomIntegerInRange(2, phi);
             while (BigInteger.GreatestCommonDivisor(e, phi) != 1)
             {
-                e = RandomIntegerInRange(2, phi);
+                e = NumberTheoryUtils.RandomIntegerInRange(2, phi);
             }
 
             var d = e.ModInverse(phi, isCoPrime: true);
             return (n, e, d);
-        }
-
-        public static BigInteger RandomPrime(long nbytes = 16)
-        {
-            var bytes = new byte[nbytes];
-            BigInteger res;
-            do
-            {
-                _rng.GetBytes(bytes);
-                res = new BigInteger(bytes);
-            } while (!res.IsProbablePrime());
-            return new BigInteger(bytes);
-        }
-
-        public static BigInteger RandomIntegerInRange(BigInteger lower, BigInteger upper)
-        {
-            var bytes = upper.ToByteArray();
-            BigInteger res;
-            do
-            {
-                _rng.GetBytes(bytes);
-                bytes[bytes.Length - 1] &= 0x7F;
-                res = new BigInteger(bytes);
-            } while (res >= upper || res <= lower);
-
-            return res;
         }
     }
 }
---
+++ YetAnotherOneRSA/NumberTheoryUtils.cs
@@ -0,0 +1,36 @@
+﻿using System.Numerics;
+using System.Security.Cryptography;
+
+namespace YetAnotherOneRSA
+{
+    public static class NumberTheoryUtils
+    {
+        private static readonly RandomNumberGenerator Rng = RandomNumberGenerator.Create();
+
+        public static BigInteger RandomPrime(long nbytes = 16)
+        {
+            var bytes = new byte[nbytes];
+            BigInteger res;
+            do
+            {
+                Rng.GetBytes(bytes);
+                res = new BigInteger(bytes);
+            } while (!res.IsProbablePrime());
+            return new BigInteger(bytes);
+        }
+
+        public static BigInteger RandomIntegerInRange(BigInteger lower, BigInteger upper)
+        {
+            var bytes = upper.ToByteArray();
+            BigInteger res;
+            do
+            {
+                Rng.GetBytes(bytes);
+                bytes[bytes.Length - 1] &= 0x7F;
+                res = new BigInteger(bytes);
+            } while (res >= upper || res <= lower);
+
+            return res;
+        }
+    }
+}
